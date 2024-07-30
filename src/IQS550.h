f69f15d14d37b84a936480f5197a323972e0b984
DigiMax <42356489+DigiMaxIO@users.noreply.github.com>
2024-07-30 08:00:11 -0700

    Update IQS550.h

--- src/IQS550.h
+++ src/IQS550.h
@@ -11,29 +11,9 @@
 #define	END_WINDOW				(uint16_t)0xEEEE
 
 #define BitIsSet(VAR,Index)		(VAR & (1<<Index)) != 0
-#define BitIsNotSet(VAR,Index)	(VAR & (1<<Index)) == 0
-
-#define SetBit(VAR,Index)		VAR |= (1<<Index)
-#define ClearBit(VAR,Index)		VAR &= (uint8_t)(~(1<<Index))
-
-#define FALSE 					0
-#define	TRUE 					!FALSE
-
-#define START           0x08
-#define REPEATED_START  0x10
-#define MT_SLA_ACK		0x18
-#define MT_SLA_NACK		0x20
-#define MT_DATA_ACK     0x28
-#define MT_DATA_NACK    0x30
-#define MR_SLA_ACK		  0x40
-#define MR_SLA_NACK		  0x48
-#define MR_DATA_ACK     0x50
-#define MR_DATA_NACK    0x58
-#define LOST_ARBTRTN    0x38
 
 #define STOP                              true    
 #define RESTART                           false
-
 
 
 typedef struct {
